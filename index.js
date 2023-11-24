794dae78b20274bf195d4a73b94ad2bba6e47c8e
Shuhel Ahmed <shuhel020@gmail.com>
2023-11-24 01:20:16 -0500

    update

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ const cors = require('cors')
 
 // app.use(cors())
 
-const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
+// const cors=require("cors");
+// const corsOptions ={
+//    origin:'*', 
+//    credentials:true,            //access-control-allow-credentials:true
+//    optionSuccessStatus:200,
+// }
 
-app.use(cors(corsOptions)) 
-
+app.use(cors({credentials: true, origin:'https://tourist-clinic-server2.vercel.app'}))
 
 app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
