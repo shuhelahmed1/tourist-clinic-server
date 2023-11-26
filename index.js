c97146605c633248ae96757bd154efb5482395be
Shuhel Ahmed <shuhel020@gmail.com>
2023-11-25 23:04:39 -0500

    update

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 
-app.use(
-  cors({
-  origin:true,
-  credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  })
-  );
+app.use (cors()) 
 
 app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
