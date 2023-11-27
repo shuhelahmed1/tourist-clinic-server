c4a0a2d8a1028a76641e9529cf4b11a4ebf1901a
Shuhel Ahmed <shuhel020@gmail.com>
2023-11-27 11:55:42 -0500

    up

--- index.js
+++ index.js
@@ -4,8 +4,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 
-app.use(cors()) 
-app.use(express.json())
+app.use(
+  cors({
+  origin:true,
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+  })
+  );app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // // Create a MongoClient with a MongoClientOptions object to set the Stable API version
