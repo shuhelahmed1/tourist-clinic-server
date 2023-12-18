91987f4e2f86e319662f89033bde4401d2d90e6e
Shuhel Ahmed <shuhel020@gmail.com>
2023-12-18 00:34:00 -0500

    cors

--- index.js
+++ index.js
@@ -2,10 +2,16 @@ const express = require('express')
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const cors = require('cors')
 
-app.use(cors());
-  app.use(express.json())
+const cors=require("cors");
+const corsOptions ={
+   origin:'*', 
+   credentials:true,            //access-control-allow-credentials:true
+   optionSuccessStatus:200,
+}
+
+app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // Replace the uri string with your MongoDB deployment's connection string.
