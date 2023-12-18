6d0f73584260e44b1ff3f8f2aefc758ab3e2db36
Shuhel Ahmed <shuhel020@gmail.com>
2023-12-18 00:49:17 -0500

    cors

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 const cors=require("cors");
-const corsOptions ={
-   origin:'*', 
-   credentials:true,            //access-control-allow-credentials:true
-   optionSuccessStatus:200,
-}
 
-app.use(cors(corsOptions)) // Use this after the variable declaration
+app.use(cors({
+  origin: '*'
+})) 
 app.use(express.json())
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
